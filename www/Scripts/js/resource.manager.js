d728d6d16d56d10e643a327fc858468d38af5fd8
potry <potry@ua.fm>
2014-11-20 16:14:43 +0200

    sdsd

--- www/Scripts/js/resource.manager.js
+++ www/Scripts/js/resource.manager.js
@@ -1,32 +1,54 @@
 ﻿
 Resource = {
     Init: function (callback) {
-        DB.selectData("Select * From MocaResources", function (results) {
-            if (results.rows.length > 0) {
-                var resourceObj = [];
-                for (var i = 0; i < results.rows.length; i++) {
-                    resourceObj.push(results.rows.item(i));
+        if (!$.session.get('MocaResources')) {
+            $('body').append('<div id="preload"></div>');
+            DB.selectData("Select * From MocaResources", function (results) {
+                if (results.rows.length > 0) {
+                    var resourceObj = [];
+                    for (var i = 0; i < results.rows.length; i++) {
+                        resourceObj.push(results.rows.item(i));
+                    }
+                    $.session.set('MocaResources', JSON.stringify(resourceObj));
+
                 }
-                $.session.set('MocaResources', JSON.stringify(resourceObj));
-
-
-            }
-            callback.apply(null);
-            $("#preload").remove();
-        });
+                callback.apply(null);
+                $("#preload").remove();
+            });
+        }
     },
     LocolizePage: function () {
-        $("[data-resource]").each(function (k, v) {
-            var data = $(this).data("resource");
-            var resValue = Resource.Value(data.name);
-            switch (data.attribute) {
-                case "html":
-                    $(this).html(resValue);
-            }
-        });
+      
+        var locolizePage = function () {
+            $("[data-resource]").each(function (k, v) {
+                var data = $(this).data("resource");
+               
+                var resValue = Resource.Value(data.name);
+               
+                switch (data.attribute) {
+                    case "html":
+                        $(this).html(resValue);
+                    case "placeholder":
+                        $(this).attr("placeholder", resValue);
+                }
+            });
+            $("#preload").remove();
+        };
+        if (!$.session.get('MocaResources')) {
+     
+            Resource.Init(function () {
+                locolizePage();
+            });
+        }
+        else {
+            
+            locolizePage();
+        };
+
     },
     Value: function (key) {
         var resources = $.session.get('MocaResources');
+        console.log(resources);
         if (resources) {
             resources = $.parseJSON(resources);
             for (var i = 0; i < resources.length; i++) {
@@ -36,15 +58,13 @@ Resource = {
             }
         }
     },
-    Update: function () {
+    Update: function (callback) {
         $.ajax({
             url: serverUrl + "/Resource/Get",
             type: "POST",
             //data: { model: e.data.queryResult, email: email },
             dataType: 'json',
-
             success: function (data) {
-                console.log(data);
                 if (data) {
                     DB.deleteData("DELETE FROM MocaResources", function () {
                         var resourceInsData =
@@ -52,10 +72,12 @@ Resource = {
                             tableName: 'MocaResources',
                             data: data
                         };
+                
 
-                      
                         DB.insertData(resourceInsData, function (res) {
-
+                            $.session.set('MocaResources', JSON.stringify(data));
+                            console.log('updateApply');
+                            callback.apply(null);
                         })
                     });
 
@@ -71,3 +93,9 @@ Resource = {
     },
 
 };
+$(document).ready(function () {
+    Resource.Update(function () {;
+        console.log('LocolizePage');
+        Resource.LocolizePage();
+    });
+});
